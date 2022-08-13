8964b8c312afe5eb25cff864cb409542162e8318
Linda-Joe <Linda13971346837@163.com>
2022-08-13 21:36:08 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <my-addaction></my-addaction>
+    <p>------------------------------</p>
+    <my-subtracaction></my-subtracaction>
+  </div>
+</template>
+<script>
+import AddAction from './components/AddAction.vue'
+import Subtracaction from './components/subtracaction.vue'
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    'my-addaction': AddAction,
+    'my-subtracaction': Subtracaction,
+  },
+}
+</script>
+<style lang="less" scoped>
+div {
+  color: red;
+}
+</style>
